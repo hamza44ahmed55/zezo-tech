6ddb66d9bfa472f44d7f91629884d278238e68a2
hamza ahmed <hamzaahmedhamed321@gmail.com>
2025-03-08 20:42:56 +0200

    hamza ahmed hamed do some changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # zezo-tech ~ from hamza ahmed hamed
-[logo](/zezo-tech/min/imges/zezo%20-%20logo%20-%20png.png)
+![logo](/min/imges/zezo%20-%20logo%20-%20png.png)
 # me
 # *hamza ahmed hamed*
 ```
--- min/DB/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE site_settings (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL
-);
-
-INSERT INTO site_settings (title, description) VALUES ('hamza ahmed', 'hamza ahmed hamed');
