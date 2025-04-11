04f3d8b5393d533a49508a1e73884c35f806885b
hamza ahmed <hamzaahmedhamed321@gmail.com>
2025-04-11 21:44:18 +0200

    hamza ahmed hamed do some changes

---
+++ hamza wep new 2025/hamza.js
@@ -0,0 +1,16 @@
+//document.addEventListener("DOMContentLoaded", function () {
+   // let sections = document.querySelectorAll(".cards, .projects");
+
+   // let observer = new IntersectionObserver((entries) => {
+     //   entries.forEach(entry => {
+     //       if (entry.isIntersecting) {
+        //        entry.target.style.opacity = 1;
+          //      entry.target.style.transform = "translateY(0)";
+            //}
+      //  });
+  //  }, { threshold: 0.3 });
+
+   // sections.forEach(section => {
+    //    observer.observe(section);
+  //  });
+//});
